df4771c804d6107f275099812ab3653b483db7e9
Cybiii <sebastiansilva@berkeley.edu>
2026-02-22 06:22:13 -0800

    verilog 95 compatible jasergold

--- axi_lite/address_decoder.sv
+++ axi_lite/address_decoder.sv
@@ -1,6 +1,6 @@
 // input a address => outputs slave id, valid, and error
 // M = number of slaves (N×M convention)
-// Verilog-2001 style for JasperGold compatibility
+// Verilog-95 compatible for JasperGold (explicit sensitivity, no variable part-select)
 module addr_decoder #(
     parameter M = 4,
     parameter ADDR_WIDTH = 32,
@@ -14,14 +14,23 @@ module addr_decoder #(
     output reg decerr
 );
 
-    integer i;
+    // Per-slave range match (constant part-select in generate => Verilog-95 ok)
+    wire [M-1:0] match;
+    genvar g;
+    generate
+        for (g = 0; g < M; g = g + 1) begin : gen_slave
+            wire [ADDR_WIDTH-1:0] base_g = BASE_ADDR_PACKED[g*ADDR_WIDTH + ADDR_WIDTH - 1 : g*ADDR_WIDTH];
+            wire [ADDR_WIDTH-1:0] size_g = SIZE_PACKED[g*ADDR_WIDTH + ADDR_WIDTH - 1 : g*ADDR_WIDTH];
+            assign match[g] = (addr >= base_g) && (addr < base_g + size_g);
+        end
+    endgenerate
 
+    integer i;
     always @(addr) begin
         slave_id = 0;
         valid = 1'b0;
         for (i = 0; i < M; i = i + 1) begin
-            if (!valid && addr >= BASE_ADDR_PACKED[i*ADDR_WIDTH +: ADDR_WIDTH] &&
-                addr < (BASE_ADDR_PACKED[i*ADDR_WIDTH +: ADDR_WIDTH] + SIZE_PACKED[i*ADDR_WIDTH +: ADDR_WIDTH])) begin
+            if (!valid && match[i]) begin
                 slave_id = i;
                 valid = 1'b1;
             end
---
+++ jgproject/sessionLogs/session_0/jg_session_0.cmd
@@ -0,0 +1,12 @@
+# ----------------------------------------
+# Jasper Version Info
+# tool      : Jasper 2024.12
+# platform  : Linux 4.18.0-553.104.1.el8_10.x86_64
+# version   : 2024.12p002 64 bits
+# build date: 2025.02.26 14:36:14 UTC
+# ----------------------------------------
+# started   : 2026-02-21 04:48:49 PST
+# hostname  : eda-1.EECS.Berkeley.EDU.(none)
+# pid       : 643395
+# arguments : '-label' 'session_0' '-console' '//127.0.0.1:40925' '-style' 'windows' '-data' 'AAAAXHicY2RgYLCp////PwMYMD6A0Aw2jAyoAMRnQhUJbEChGRhYUbUAAL9fBoQ=' '-proj' '/scratch/cs199-akc/NoC-experiments/jgproject/sessionLogs/session_0' '-init' '-hidden' '/scratch/cs199-akc/NoC-experiments/jgproject/.tmp/.initCmds.tcl'
+analyze -verilog {/scratch/cs199-akc/NoC-experiments/axi_lite/address_decoder.sv} ; analyze -verilog {/scratch/cs199-akc/NoC-experiments/axi_lite/bridge_1x1.sv} ; analyze -verilog {/scratch/cs199-akc/NoC-experiments/axi_lite/bridge_1xM.sv} ; analyze -verilog {/scratch/cs199-akc/NoC-experiments/axi_lite/bridge_Nx1.sv} ; analyze -verilog {/scratch/cs199-akc/NoC-experiments/axi_lite/fifo.sv} ; analyze -verilog {/scratch/cs199-akc/NoC-experiments/axi_lite/interface.sv} ; analyze -verilog {/scratch/cs199-akc/NoC-experiments/axi_lite/rr_arbiter.sv} ; 
